b15ff5d16eea4de7021e8d5088f78cc5566d7072
Alan Palazzolo <apalazzolo@minnpost.com>
2014-09-22 15:32:13 -0500

    Making district map area smaller.

--- js/app.js
+++ js/app.js
@@ -167,7 +167,7 @@ define('minnpost-hot-house-districts-2014', [
         style: mpMaps.mapStyle
       }).addTo(map);
       map.fitBounds(layer.getBounds(), {
-        padding: [25, 25]
+        padding: [65, 65]
       });
       map.invalidateSize();
 
